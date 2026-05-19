6270efb06449d47feba46ee200d2e08454493b44
kuronosu1024 <kurocyan1024@gmail.com>
2026-05-19 22:38:37 +0900

    modified:   .github/workflows/verify.yml new file:   library/template/template.hpp modified:   verify/unionfind.test.cpp

---
+++ library/template/template.hpp
@@ -0,0 +1,9 @@
+#pragma once
+using namespace std;
+#include <bits/stdc++.h>
+
+
+using ll = long long;
+using ld = long double;
+
+#include "macro.hpp"
--- verify/unionfind.test.cpp
+++ verify/unionfind.test.cpp
@@ -1,5 +1,9 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
 
+#include "../library/template/template.hpp"
+
+#include "../library/datastructure/unionfind.hpp"
+
 #include <bits/stdc++.h>
 using namespace std;
  
@@ -7,48 +11,6 @@ using ll = long long;
 using ld = long double;
 #define rep(i,n) for(i=0;i<(n);i++)
 #define all(a) a.begin(), a.end()
-#define min(a,b) (a<b?a:b)
-#define max(a,b) (a>b?a:b)
-
-namespace nskr{
-    int i, j;
-    struct unionfind{
-        //unionfind - union by size, 経路圧縮
-        vector<int> pr, sz;
-        unionfind(int n){
-            pr.resize(n);
-            sz.resize(n,1);
-            rep(i,n) pr[i] = i;
-        }
-
-        inline int find(int x){ 
-            while(pr[x] != x){ 
-                pr[x] = pr[pr[x]];
-                x = pr[x];
-            }
-            return x;
-        }
-        
-        inline void merge(int p, int q){ 
-            p = find(p); q = find(q);
-            if(p==q) return ;
-            if(sz[p] < sz[q]) swap(p,q);
-            pr[q] = p;
-            sz[p] += sz[q];
-            return ;
-        }
-
-        inline bool same(int p, int q){
-            if(find(p) == find(q)) return true;
-            return false;
-        }
-
-        int size(int x){
-            return sz[find(x)];
-        }
-    };
-}
-
 
 
 using namespace nskr;

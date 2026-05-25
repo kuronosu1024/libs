f8ef484db641f797c86fcd3ed0b8df29006a62c4
kuronosu1024 <kurocyan1024@gmail.com>
2026-05-25 16:16:36 +0900

    Update binomial_coefficient_prime_mod.test.cpp

--- verify/yosupo/binomial_coefficient_prime_mod.test.cpp
+++ verify/yosupo/binomial_coefficient_prime_mod.test.cpp
@@ -1,7 +1,7 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/binomial_coefficient_prime_mod"
 
 #include "../../library/template/template.hpp"
-#include "../../library/math/convolution.hpp"
+#include "../../library/math/combination.hpp"
 
 using namespace nskr;
 int main(void){
@@ -10,10 +10,10 @@ int main(void){
     ios::sync_with_stdio(false);
     int t, m; cin >> t >> m;
     modint<0> x;x.set_mod(m);
-    convolution<modint<0>> c;
+    combination<modint<0>> c;
     for(;t--;){
         int n,k; cin >> n >> k;
         c.preculc(max(n,k));
-        cout << c.conv(n,k) << "\n";
+        cout << c.comb(n,k) << "\n";
     }
 }
